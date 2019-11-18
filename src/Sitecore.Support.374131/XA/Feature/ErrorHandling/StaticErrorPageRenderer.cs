bb836bcdad7ab2a6cc5776e7f9d781b7dcabcdf5
Anton Kechashin <atk@sitecore.net>
2019-11-18 14:51:39 +0200

    Fix for XP 9.0.2 / SXA 1.8.1

---
+++ src/Sitecore.Support.374131/XA/Feature/ErrorHandling/StaticErrorPageRenderer.cs
@@ -0,0 +1,40 @@
+﻿namespace Sitecore.Support.XA.Feature.ErrorHandling
+{
+    using Sitecore.Configuration;
+    using Sitecore.Data.Items;
+    using Sitecore.Links;
+    using Sitecore.Web;
+    using System;
+
+    public class StaticErrorPageRenderer : Sitecore.XA.Feature.ErrorHandling.StaticErrorPageRenderer
+    {
+        protected override string GetItemUrl(Item item, SiteInfo site)
+        {
+            UrlOptions options = LinkManager.GetDefaultUrlOptions();
+            options.AlwaysIncludeServerUrl = true;
+            options.Site = Factory.GetSite(site.Name);
+            
+            string itemUrl = LinkManager.GetItemUrl(item, options);
+            if (itemUrl.StartsWith("://", StringComparison.Ordinal))
+            {
+                itemUrl = "http" + itemUrl;
+            }
+            #region Added code
+            Uri host = new Uri(itemUrl);
+            string[] hostNames = site.HostName.Split(new char[] {'|'});
+            foreach (var hostName in hostNames)
+            {
+                if (!hostName.Contains("*"))
+                {
+                    return host.Scheme + "://" + hostName + host.PathAndQuery;
+                }
+            }
+            if (site.Matches(WebUtil.GetHostName()) && !string.IsNullOrEmpty(site.TargetHostName))
+            {
+                return host.Scheme + "://" + WebUtil.GetHostName() + host.PathAndQuery;
+            }
+            #endregion
+            return itemUrl;
+        }
+    }
+}
